0046460d2add6c0ff6202f2aa7e0febec6b000c1
Seongkyun-Yu <ysungkyun@gmail.com>
2020-02-18 17:40:34 +0900

    spread 공부

---
+++ javascript_study/2020-02-18/spread.js
@@ -0,0 +1,17 @@
+const slime = {
+    name: '슬라임'
+};
+
+const cuteSlime = {
+    ...slime,
+    attribute: 'cute'
+};
+
+const purpleCuteSlime = {
+    ...cuteSlime,
+    color: 'purple'
+};
+
+console.log(slime);
+console.log(cuteSlime);
+console.log(purpleCuteSlime);
