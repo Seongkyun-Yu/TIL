c2a5bd68a4be497db0f6bce5ec31b04f92866236
Seongkyun-Yu <ysungkyun@gmail.com>
2020-02-18 21:30:16 +0900

    rest 공부

--- javascript_study/2020-02-17/array_test.js
+++ javascript_study/2020-02-17/array_test.js
@@ -2,13 +2,11 @@ function countBiggerThanTen(numbers) {
     /* 구현해보세요 */
     return numbers.reduce((acc,current) => {
         if(current>10)
-            acc.push(current)
-        else
-            continue
+            acc.push(current);
+        return acc;
     }, [])
   }
   
   const count = countBiggerThanTen([1, 2, 3, 5, 10, 20, 30, 40, 50, 60]);
   console.log(count); // 5
   
-  export default countBiggerThanTen;
--- javascript_study/2020-02-18/rest.js
+++ javascript_study/2020-02-18/rest.js
@@ -7,3 +7,13 @@ const purpleCuteSlime = {
 const {color, ...fdfs} = purpleCuteSlime;
 console.log(color);
 console.log(fdfs);
+
+
+function sum(...rest) {
+    return rest.reduce((acc, current) => acc+current, 0)
+}
+
+console.log(sum(1,2,3,4,5,6,7,8))
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+console.log(sum(numbers))
