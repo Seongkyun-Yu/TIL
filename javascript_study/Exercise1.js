bdfc601dd1b8fe52ef1875e0f4c4854a160c2c8e
Seongkyun-Yu <ysungkyun@gmail.com>
2020-02-26 23:04:06 +0900

    연습문제 추가풀이

--- javascript_study/Exercise1.js
+++ javascript_study/Exercise1.js
@@ -1,10 +1,11 @@
 // 제어문 연습 문제
 
 // 1. 변수 x가 10보다 크고 20보다 작을 때 변수 X를 출력하는 조건식을 완성하라
-var x = 15;
+let x = 15;
 
 if (x > 10 && x < 20)
   console.log(x);
+
 
 // 2. for문을 사용하여 0부터 10미만의 정수 중에서 짝수만을 작은 수부터 출력하시오.
 for (let i = 0; i < 10; i++) {
@@ -12,13 +13,15 @@ for (let i = 0; i < 10; i++) {
     console.log(i);
 }
 
+
 // 3. for문을 사용하여 0부터 10미만의 정수 중에서 짝수만을 작은 수부터 문자열로 출력하시오.
-var str = "";
+let str3 = "";
 for (let i = 0; i < 10; i++){
   if ( i % 2 == 0 )
-    str += i;
+  str3 += i;
 }
-console.log(str)
+console.log(str3);
+
 
 // 4. for문을 사용하여 0부터 10미만의 정수 중에서 홀수만을 큰수부터 출력하시오.
 for (let i = 9; i >= 0; i--) {
@@ -26,13 +29,15 @@ for (let i = 9; i >= 0; i--) {
     console.log(i);
 }
 
+
 // 5. while문을 사용하여 0 부터 10 미만의 정수 중에서 짝수만을 작은 수부터 출력하시오.
-var i = 0
-while ( i < 10 ){
-  if (i % 2 == 0)
-    console.log(i);
-  i++;
+let q5_i = 0
+while ( q5_i < 10 ){
+  if (q5_i % 2 == 0)
+    console.log(q5_i);
+  q5_i++;
 }
+
 
 // 6. while문을 사용하여 0 부터 10 미만의 정수 중에서 홀수만을 큰수부터 출력하시오.
 var i = 10
@@ -42,28 +47,29 @@ while ( i > 0 ){
   i--;
 }
 
+
 // 7. for 문을 사용하여 0부터 10미만의 정수의 합을 출력하시오.
-var sum = 0;
+let sum7 = 0;
 for (let i = 0; i < 10; i++) {
-  sum += i;
+  sum7 += i;
 }
-console.log(sum)
+console.log(sum7);
 
 
 // 8. 1부터 20 미만의 정수 중에서 2 또는 3의 배수가 아닌 수의 총합을 구하시오.
-var sum = 0;
+let sum8 = 0;
 for (let i = 1; i < 20; i++) {
-  sum += i % 2 ? (i % 3 ? i : 0) : 0;
+  sum8 += i % 2 ? (i % 3 ? i : 0) : 0;
 }
-console.log(sum)
+console.log(sum8);
 
 
 // 9. 1부터 20 미만의 정수 중에서 2 또는 3의 배수인 수의 총합을 구하시오.
-var sum = 0;
+let sum9 = 0;
 for (let i = 1; i < 20; i++) {
-  sum += i % 2 ? (i % 3 ? 0 : i) : i;
+  sum9 += i % 2 ? (i % 3 ? 0 : i) : i;
 }
-console.log(sum);
+console.log(sum9);
 
 
 // 10. 두 개의 주사위를 던졌을 때, 눈의 합이 6이 되는 모든 경우의 수를 출력하시오.
@@ -76,86 +82,113 @@ for(let i=1; i<7; i++){
 
 
 // 11. 삼각형 출력하기 - pattern 1
-var str = '// 높이(line)가 5 \n';
-var addstr = '*';
+let str11 = '// 11. 삼각형 출력하기 - pattern 1 \n';
+let addstr11 = '*';
 for(let i=0; i<5; i++){
   for(let j=0; j<i+1; j++){
-    str += addstr;
+    str11 += addstr11;
   }
-  str += '\n';
+  str11 += '\n';
 }
-console.log(str)
+console.log(str11);
 
 // 12. 삼각형 출력하기 - pattern 2
-var str = '*****\n';
-var addstr = '';
-for(let i=5; i>0; i--){
-  addstr += ' ';
-  str += addstr;
-  for(let j=i-1; j>0; j--){
-    str += '*'
-  }
-  str += '\n';
-}
-console.log(str)
+let str12_v1 = '// 12. 삼각형 출력하기 - pattern 2 v1 \n';
+let addstr12_v1 = '';
+for(let i=0; i < 5 ; i++){
+  addstr12_v1 += i ? ' ' : ''
 
-var str = '';
-var addstr = '';
-for(let i=0; i<5; i++){
-  addstr += '*';
-  for(let j=0; j<i-5; j++){
-    str += ' '
+  str12_v1 += addstr12_v1;
+
+  for(let j= i; j<5; j++){
+    str12_v1 += '*';
   }
-  str += addstr;
-  str += '\n';
+  str12_v1 += '\n';
 }
-console.log(str)
+console.log(str12_v1)
+
+let str12_v2 = '// 12. 삼각형 출력하기 - pattern 2 v2 \n';
+for(let i=0; i < 5 ; i++){
+  for(let j=i; j > 0; j--){
+    str12_v2 += ' ';
+  }
+  for(let j= i; j<5; j++){
+    str12_v2 += '*';
+  }
+  str12_v2 += '\n'
+}
+console.log(str12_v2);
+
 
 // 13. 삼각형 출력하기 - pattern 3
-var str = '';
-for(let i=5; i>=0; i--){
-  for(let j=i; j>0; j--){
-    str += '*'
+let str13_v1 = '// 13. 삼각형 출력하기 - pattern 3 v1 \n';
+for(let i=0; i<5; i++){
+  for(let j=0; j<5-i; j++){
+    str13_v1 += '*'
   }
-  str += '\n';
+  str13_v1 += '\n';
 }
-console.log(str)
+console.log(str13_v1);
+
+let str13_v2 = '// 13. 삼각형 출력하기 - pattern 3 v2 \n';
+for(let i=5; i>0; i--){
+  for(let j=i; j>0; j--){
+    str13_v2 += '*'
+  }
+  str13_v2 += '\n';
+}
+console.log(str13_v2);
 
 // 14. 삼각형 출력하기 - pattern 4
-var str = '';
-var addstr = '';
+let str14 = '// 14. 삼각형 출력하기 - pattern 4 \n';
+let addstr14 = '';
 for(let i=5; i>0; i--){
   for(let j=i-1; j>0; j--){
-    str += ' '
+    str14 += ' '
   }
-  addstr += '*';
-  str += addstr;
-  str += '\n';
+  addstr14 += '*';
+  str14 += addstr14;
+  str14 += '\n';
 }
-console.log(str)
+console.log(str14);
 
 // 15. 정삼각형 출력하기
-var str = '';
+let str15 = '// 15. 정삼각형 출력하기 \n';
 for(let i=0; i<5; i++){
   for(let j=4-i; j>0; j--){
-    str += ' '
+    str15 += ' '
   }
   for(let k=0; k<2*i+1; k++){
-    str += '*';
+    str15 += '*';
   }
-  str += '\n';
+  str15 += '\n';
 }
-console.log(str)
+console.log(str15);
 
 // 16. 정삼각형 출력하기
-var str = '';
+let str16_v1 = '// 15. 정삼각형 출력하기 (역방향) v1 \n';
+let addstr16_v1 = '';
 for(let i=0; i<5; i++){
-  for(let k=0; k<i; k++){
-    str += ' ';
+  addstr16_v1 += i ? ' ' : '';
+
+  str16_v1 += addstr16_v1;
+
+  for(let k=9-2*i; k>0; k--){
+    str16_v1 += '*'
   }
-  for(let j=9-2*i; j>0; j--){
-    str += '*'
-  }  
-  str += '\n';
+
+  str16_v1 += '\n';
 }
-console.log(str)
+console.log(str16_v1);
+
+let str16_v2 = '// 15. 정삼각형 출력하기 (역방향) v2 \n';
+for(let i=0; i<5; i++){
+  for(let j=0; j<i; j++){
+    str16_v2 += ' ';
+  }
+  for(let k=9-2*i; k>0; k--){
+    str16_v2 += '*'
+  }  
+  str16_v2 += '\n';
+}
+console.log(str16_v2);
