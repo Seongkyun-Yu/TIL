2dcd0fd8962676682aaf25bd414774c2ec7c625a
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-02 23:34:53 +0900

    prototype 연습2

--- javascript_study/prototype.js
+++ javascript_study/prototype.js
@@ -1,7 +1,13 @@
 // 함수 정의(constructor)가 평가되어 함수 객체를 생성하는 시점에 프로토타입도 더불어 생성된다.
-console.log(Person.prototype); // {constructor: ƒ}
+console.log(Person.prototype); // 선언 돼었으나 할당 되지 않아서 constructor 없음
+console.log(Person2.prototype); // {constructor: ƒ}
 
 // 생성자 함수
-var Person = function (name) {
+let Person = function (name) {
+  this.name = name;
+}
+
+// 생성자 함수
+function Person2 (name) {
   this.name = name;
 }
