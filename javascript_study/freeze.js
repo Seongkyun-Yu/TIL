1f5f564ec780cfd7d7bca4713a81f8d8db5ecb56
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-10 15:35:27 +0900

    Object Freeze

---
+++ javascript_study/freeze.js
@@ -0,0 +1,21 @@
+// 배열함수를 쓰지 않고 Freeze 시키기
+function deepFreeze(target) {
+  if (target && typeof target === 'object' && !Object.isFrozen(target)) {
+    Object.freeze(target);
+    let keyArr = Object.keys(target);
+    for (let i = 0; i < keyArr.length; i++)
+      deepFreeze(target[keyArr[i]]);
+  }
+}
+
+const person = {
+  name: 'Lee',
+  home: { city: 'Seoul' },
+  company: { city: 'Incheon' }
+};
+
+
+deepFreeze(person);
+
+console.log(Object.isFrozen(person)); // true
+console.log(Object.isFrozen(person.home));
