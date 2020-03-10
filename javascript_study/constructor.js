bede5d6b15a38c1e1c0d9586edb7fe8bf72b663d
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-10 22:51:31 +0900

    constructor example

--- javascript_study/constructor.js
+++ javascript_study/constructor.js
@@ -26,3 +26,28 @@ function createUser(name, role) {
 let inst = createUser('Lee', 'admin');
 // 함수가 생성한 객체를 반환한다.
 console.log(inst); // {name: "Lee", role: "admin"}
+
+
+function Circle1(radius) {
+  // 생성자 함수가 new 연산자와 함께 호출되면 함수의 선두에서 빈 객체를 생성하고
+  // this에 바인딩한다. 이때 this와 Circle은 프로토타입에 의해 연결된다.
+  // if (Circle1.prototype === this)
+  //   console.log('같음')
+  console.log(this.__proto__ === Circle1.prototype)
+
+  // 이 함수가 new 연산자와 함께 호출되지 않았다면 이 시점의 this는 전역 객체 window를 가리킨다.
+  // 즉, this와 Circle은 프로토타입에 의해 연결되지 않는다.
+  if (!(this instanceof Circle1)) {
+    // new 연산자와 함께 호출하여 생성된 인스턴스를 반환한다.
+    return new Circle1(radius);
+  }
+
+  this.radius = radius;
+  this.getDiameter = function () {
+    return 2 * this.radius;
+  };
+
+  console.log(this)
+}
+
+const ting = new Circle1(2);
