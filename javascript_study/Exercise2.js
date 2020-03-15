7917c2da22aaff6b5f426de0d53d3e5f3af5ed4a
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-15 18:35:11 +0900

    알고리즘 문제 개선

---
+++ javascript_study/Exercise2.js
@@ -0,0 +1,63 @@
+function linearSearch(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 5)); // 4
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(linearSearch([1, 2, 3, 4, 5, 6], -1)); // -1
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 0)); // -1
+console.log(linearSearch([1, 2, 3, 4, 5, 6], 7)); // -1
+
+
+
+function binarySearch(array, target) {
+  let startIndex = 0;
+  let endIndex = array.length - 1;
+  let midIndex;
+
+  while (startIndex <= endIndex) {
+    midIndex = Math.floor((startIndex + endIndex) / 2);
+    if (array[midIndex] < target) {
+      startIndex = ++midIndex;
+    } else if (array[midIndex] > target) {
+      endIndex = --midIndex;
+    } else {
+      return midIndex;
+    }
+  }
+  return -1;
+}
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 1)); // 0
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 3)); // 2
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 5)); // 4
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 6)); // 5
+console.log(binarySearch([1, 2, 3, 4, 5, 6], -1)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 0)); // -1
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 7)); // -1
+
+
+
+function bubbleSort(array) {
+  let replace;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      if (array[j] > array[j + 1]) {
+        replace = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = replace;
+      }
+    }
+  }
+  return array;
+}
+
+console.log(bubbleSort([2, 4, 5, 1, 3]));     // [1, 2, 3, 4, 5]
+console.log(bubbleSort([5, 2, 1, 3, 4, 6]));  // [1, 2, 3, 4, 5, 6]
+console.log(bubbleSort([3, 1, 0, 11, 4, 2, 9, -1])); // [-1, 0, 1, 2, 3, 4]
