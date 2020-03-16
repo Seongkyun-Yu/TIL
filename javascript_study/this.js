701a90ee2596e3e4a6d81635a53d66f209a5dfad
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-16 21:56:20 +0900

    feat: Study This

--- javascript_study/this.js
+++ javascript_study/this.js
@@ -1,136 +1,154 @@
-// var 키워드로 선언한 변수 value는 전역 객체의 프로퍼티이다.
+// // var 키워드로 선언한 변수 value는 전역 객체의 프로퍼티이다.
+// var value = 1;
+// // const 키워드로 선언한 변수 value는 전역 객체의 프로퍼티가 아니다.
+// // const value = 1;
+
+// const obj = {
+//   value: 100,
+//   foo: function foo () {
+//     console.log("foo's this: ", this);  // {value: 100, foo: ƒ}
+//     console.log("foo's this.value: ", this.value); // 100
+
+//     // 메소드 내에서 정의한 중첩 함수
+//     function bar() {
+//       console.log("bar's this: ", this); // window
+//       console.log("bar's this.value: ", this.value); // 1
+//     }
+
+//     // 메소드 내에서 정의한 중첩 함수도 일반 함수로 호출되면
+//     // 중첩 함수 내부의 this에는 전역 객체가 바인딩된다.
+//     bar();
+//   }
+// };
+
+// obj.foo();
+
+
+// function Person(name) {
+//   this.name = name;
+// }
+
+// Person.prototype.getName = function () {
+//   return this.name;
+// };
+
+// const me = new Person('Lee');
+// // getName 메소드를 호출한 객체는 me이다.
+// console.log(me.getName()); // ① Lee
+
+// Person.prototype.name = 'Kim';
+// // getName 메소드를 호출한 객체는 Person.prototype이다.
+// console.log(Person.prototype.getName()); // ② Kim
+
+
+// // 생성자 함수
+// function Circle(radius) {
+//   // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
+//   this.radius = radius;
+//   this.getDiameter = function () {
+//     return 2 * this.radius;
+//   };
+// }
+
+// // 인스턴스의 생성
+// // 반지름이 5인 Circle 객체를 생성
+// const circle1 = new Circle(5);
+// // 반지름이 10인 Circle 객체를 생성
+// const circle2 = new Circle(10);
+
+// console.log(circle1.getDiameter()); // 10
+// console.log(circle2.getDiameter()); // 20
+
+// // new 연산자와 함께 호출하지 않으면 생성자 함수로 동작하지 않는다.
+// // 즉, 일반적인 함수의 호출이다.
+// const circle3 = Circle(15);
+
+// // 일반 함수 Circle은 반환문이 없으므로 암묵적으로 undefined를 반환한다.
+// console.log(circle3); // undefined
+
+// // 일반 함수 Circle내의 this는 전역 객체를 가리킨다.
+// console.log(radius); // 15
+
+// function convertArgsToArray() {
+//   console.log(arguments);
+
+//   // arguments 객체를 배열로 변환
+//   // slice: 배열의 특정 부분에 대한 복사본을 생성한다.
+//   const arr = Array.prototype.slice.apply(arguments);
+//   // const arr = Array.prototype.slice.call(arguments);
+//   console.log(arr);
+
+//   return arr;
+// }
+
+// convertArgsToArray(1, 2, 3); // [ 1, 2, 3 ]
+
+
+// function getThisBinding(t) {
+//   console.log('여길봐',t);
+//   return this;
+// }
+
+// // this로 사용할 객체
+// const thisArg = { a: 1 };
+// let a = {
+//   1: 1,
+//   2: 2,
+//   3: 3
+// };
+// // 함수(getThisBinding)를 호출하면서 인수로 전달한 객체를 호출한 함수의 this에 바인딩한다.
+// // apply 메소드는 호출할 함수의 인수를 배열로 묶어 전달한다.
+// console.log(getThisBinding.apply(thisArg, a));
+// // Arguments(3) [1, 2, 3, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+// // { a: 1 }
+
+// // call 메소드는 호출할 함수의 인수를 쉼표로 구분한 리스트 형식으로 전달한다.
+// console.log(getThisBinding.call(thisArg, 1, 2, 3));
+// // Arguments(3) [1, 2, 3, callee: ƒ, Symbol(Symbol.iterator): ƒ]
+// // { a: 1 }
+
+
+// function Person1(name) {
+//   this.name = name;
+//   this.getThis = function() {
+//     console.log(this);
+//   }
+// }
+
+// Person1.prototype.doSomething = function (callback) {
+//   // ①
+//   console.log(this);
+//   callback();
+// };
+
+// function foo1() {
+//   console.log(this.name); // ②
+// }
+
+// const person = new Person1('Lee');
+
+// person.doSomething(foo1); // ''
+// // =>  window.name은 브라우저 창의 이름을 나타내는 빌트인 프로퍼티이다. window.name의 기본값은 ''이다.
+// // 만약 Node.js 환경에서 실행하면 undefined가 출력된다.
+
+// Person1.prototype.doSomething(foo1);
+
+// person.getThis();
+
+
+
 var value = 1;
-// const 키워드로 선언한 변수 value는 전역 객체의 프로퍼티가 아니다.
-// const value = 1;
 
 const obj = {
   value: 100,
-  foo: function foo () {
-    console.log("foo's this: ", this);  // {value: 100, foo: ƒ}
-    console.log("foo's this.value: ", this.value); // 100
-
-    // 메소드 내에서 정의한 중첩 함수
-    function bar() {
-      console.log("bar's this: ", this); // window
-      console.log("bar's this.value: ", this.value); // 1
-    }
-
-    // 메소드 내에서 정의한 중첩 함수도 일반 함수로 호출되면
-    // 중첩 함수 내부의 this에는 전역 객체가 바인딩된다.
-    bar();
+  foo() {
+    console.log("foo's this: ", this); // {value: 100, foo: ƒ}
+    
+    setTimeout(function () {
+      console.log("callback's this: ", this); //// 콜백 함수 내부의 this에는 전역 객체가 바인딩된다. window
+      console.log("callback's this.value: ", this.value); // 1
+    }, 100);
   }
 };
 
 obj.foo();
-
-
-function Person(name) {
-  this.name = name;
-}
-
-Person.prototype.getName = function () {
-  return this.name;
-};
-
-const me = new Person('Lee');
-// getName 메소드를 호출한 객체는 me이다.
-console.log(me.getName()); // ① Lee
-
-Person.prototype.name = 'Kim';
-// getName 메소드를 호출한 객체는 Person.prototype이다.
-console.log(Person.prototype.getName()); // ② Kim
-
-
-// 생성자 함수
-function Circle(radius) {
-  // 생성자 함수 내부의 this는 생성자 함수가 생성할 인스턴스를 가리킨다.
-  this.radius = radius;
-  this.getDiameter = function () {
-    return 2 * this.radius;
-  };
-}
-
-// 인스턴스의 생성
-// 반지름이 5인 Circle 객체를 생성
-const circle1 = new Circle(5);
-// 반지름이 10인 Circle 객체를 생성
-const circle2 = new Circle(10);
-
-console.log(circle1.getDiameter()); // 10
-console.log(circle2.getDiameter()); // 20
-
-// new 연산자와 함께 호출하지 않으면 생성자 함수로 동작하지 않는다.
-// 즉, 일반적인 함수의 호출이다.
-const circle3 = Circle(15);
-
-// 일반 함수 Circle은 반환문이 없으므로 암묵적으로 undefined를 반환한다.
-console.log(circle3); // undefined
-
-// 일반 함수 Circle내의 this는 전역 객체를 가리킨다.
-console.log(radius); // 15
-
-function convertArgsToArray() {
-  console.log(arguments);
-
-  // arguments 객체를 배열로 변환
-  // slice: 배열의 특정 부분에 대한 복사본을 생성한다.
-  const arr = Array.prototype.slice.apply(arguments);
-  // const arr = Array.prototype.slice.call(arguments);
-  console.log(arr);
-
-  return arr;
-}
-
-convertArgsToArray(1, 2, 3); // [ 1, 2, 3 ]
-
-
-function getThisBinding(t) {
-  console.log('여길봐',t);
-  return this;
-}
-
-// this로 사용할 객체
-const thisArg = { a: 1 };
-let a = {
-  1: 1,
-  2: 2,
-  3: 3
-};
-// 함수(getThisBinding)를 호출하면서 인수로 전달한 객체를 호출한 함수의 this에 바인딩한다.
-// apply 메소드는 호출할 함수의 인수를 배열로 묶어 전달한다.
-console.log(getThisBinding.apply(thisArg, a));
-// Arguments(3) [1, 2, 3, callee: ƒ, Symbol(Symbol.iterator): ƒ]
-// { a: 1 }
-
-// call 메소드는 호출할 함수의 인수를 쉼표로 구분한 리스트 형식으로 전달한다.
-console.log(getThisBinding.call(thisArg, 1, 2, 3));
-// Arguments(3) [1, 2, 3, callee: ƒ, Symbol(Symbol.iterator): ƒ]
-// { a: 1 }
-
-
-function Person1(name) {
-  this.name = name;
-  this.getThis = function() {
-    console.log(this);
-  }
-}
-
-Person1.prototype.doSomething = function (callback) {
-  // ①
-  console.log(this);
-  callback();
-};
-
-function foo1() {
-  console.log(this.name); // ②
-}
-
-const person = new Person1('Lee');
-
-person.doSomething(foo1); // ''
-// =>  window.name은 브라우저 창의 이름을 나타내는 빌트인 프로퍼티이다. window.name의 기본값은 ''이다.
-// 만약 Node.js 환경에서 실행하면 undefined가 출력된다.
-
-Person1.prototype.doSomething(foo1);
-
-person.getThis();
