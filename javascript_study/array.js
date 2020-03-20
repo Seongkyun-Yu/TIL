21117cfa1b4799f3a53a10fc5c9e7d0c9e3293a1
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-20 17:47:34 +0900

    feat: Study Array

--- javascript_study/array.js
+++ javascript_study/array.js
@@ -9,3 +9,29 @@ console.log(arr3);
 
 const arr4 = Array.of(1); // 요소가 하나여도 배열의 요소로 만듦
 console.log(arr4);
+
+
+// 스택
+const Stack = (function () {
+  function Stack(array = []) {
+    if (!Array.isArray(array)) {
+      throw new TypeError(`${array} is not an array.`);
+    }
+    this.array = array;
+  }
+
+  // 스택의 가장 마지막에 데이터를 밀어 넣는다.
+  Stack.prototype.push = function (value) {
+    return this.array.push(value);
+  };
+
+  // 스택의 가장 마지막 데이터, 즉 가장 나중에 밀어 넣은 최신 데이터를 꺼낸다.
+  Stack.prototype.pop = function () {
+    return this.array.pop();
+  };
+
+  return Stack;
+}());
+
+const stack = new Stack([1, 2]);
+console.log(stack); // [1, 2]
