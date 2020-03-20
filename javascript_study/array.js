c7f6acbdb0a3a2d12175f22d32f253809ce4b3b1
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-20 20:04:27 +0900

    feat: Make Stack function

--- javascript_study/array.js
+++ javascript_study/array.js
@@ -11,27 +11,69 @@ const arr4 = Array.of(1); // 요소가 하나여도 배열의 요소로 만듦
 console.log(arr4);
 
 
-// 스택
-const Stack = (function () {
-  function Stack(array = []) {
-    if (!Array.isArray(array)) {
-      throw new TypeError(`${array} is not an array.`);
-    }
+// 스택 기능 구현
+const Stack = (function() {
+  const Stack = function(array) {
+    if(!Array.isArray(array)) throw new Error (`${array} is not Array`);
+
     this.array = array;
   }
 
-  // 스택의 가장 마지막에 데이터를 밀어 넣는다.
-  Stack.prototype.push = function (value) {
-    return this.array.push(value);
-  };
+  Stack.prototype.push = function(n) {
+    const len = this.array.push(n);
 
-  // 스택의 가장 마지막 데이터, 즉 가장 나중에 밀어 넣은 최신 데이터를 꺼낸다.
-  Stack.prototype.pop = function () {
-    return this.array.pop();
-  };
+    console.log(`${n}을/를 배열에 넣었습니다. 현재 배열: ${this.array} 배열의 길이: ${len}`);
+
+    return len;
+  }
+
+  Stack.prototype.pop = function() {
+    const lastElement = this.array.pop();
+
+    console.log(`마지막 배열 요소(${lastElement})을/를 제거했습니다. 현재 배열: ${this.array}`);
+
+    return lastElement;
+  }
 
   return Stack;
 }());
 
-const stack = new Stack([1, 2]);
-console.log(stack); // [1, 2]
+const stack = new Stack([1, 2, 3, 4, 5, 6, 7, 8]);
+
+console.log(stack); //Stack { array: [1, 2, 3, 4, 5, 6, 7, 8] }
+
+stack.push(9); // 9을/를 배열에 넣었습니다. 현재 배열: 1,2,3,4,5,6,7,8,9 배열의 길이: 9
+stack.pop(); // 마지막 배열 요소(9)을/를 제거했습니다. 현재 배열: 1,2,3,4,5,6,7,8
+
+
+// 클래스로 스택 기능 구현
+class ClassStack {
+  constructor(array) {
+    if (!Array.isArray(array)) throw new Error(`${this.array} is not Array`);
+
+    this.array = array;
+  }
+
+  push(n) {
+    const len = this.array.push(n);
+    console.log(`${n}을/를 배열에 넣었습니다. 현재 배열: ${this.array} 배열의 길이: ${len}`);
+
+    return len;
+  }
+
+  pop(n) {
+    const lastElement = this.array.pop(n);
+    console.log(`마지막 배열 요소(${lastElement})을/를 제거했습니다. 현재 배열: ${this.array}`);
+
+    return lastElement;
+  }
+}
+
+const classStack = new ClassStack([1, 2, 3, 4, 5, 6, 7, 8]);
+
+console.log(classStack); // ClassStack { array: [1, 2, 3, 4, 5, 6, 7, 8] }
+classStack.push(9); // 9을/를 배열에 넣었습니다. 현재 배열: 1,2,3,4,5,6,7,8,9 배열의 길이: 9
+classStack.pop(); // 마지막 배열 요소(9)을/를 제거했습니다. 현재 배열: 1,2,3,4,5,6,7,8
+
+
+
