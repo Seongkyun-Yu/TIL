e0c8a4af2a0cd57ddeb5c7cb81f4d406c3258829
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-23 16:17:47 +0900

    feat: Make forEach Example

--- javascript_study/array.js
+++ javascript_study/array.js
@@ -85,3 +85,39 @@ console.log(result); // true
 // 배열에 요소 100이 포함되어 있는지 확인한다.
 result = arr.includes(100);
 console.log(result); // false
+
+
+// forEach를 활용한 html 만들기
+const todos = [
+  { id: 3, content: 'HTML', completed: false },
+  { id: 2, content: 'CSS', completed: true },
+  { id: 1, content: 'Javascript', completed: false }
+];
+
+
+function render() {
+  let html = '';
+
+  todos.forEach((todo) => {
+    html += `<li id="${todo.id}">
+  <label><input type="checkbox"${todo.completed ? ' checked' : ''}>${todo.content}</label>
+</li>
+`;
+  });
+
+  return html;
+}
+
+console.log(render());
+
+/*
+<li id="3">
+  <label><input type="checkbox">HTML</label>
+</li>
+<li id="2">
+  <label><input type="checkbox" checked>CSS</label>
+</li>
+<li id="1">
+  <label><input type="checkbox">Javascript</label>
+</li>
+*/
