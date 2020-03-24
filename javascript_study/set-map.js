f4bf0d4f7558d178fc3ff0a83468737b6a4a3664
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-24 15:14:46 +0900

    feat: Make Union Func

--- javascript_study/set-map.js
+++ javascript_study/set-map.js
@@ -63,3 +63,30 @@ console.log(getAllTypeUnion(set1, set2)); // [ 1, 2, 3, 4, 5, 6, 7 ]
 
 
 
+// Set 객체 받아 차집합 구하기
+const getDifference = function(set1, set2) {
+  let result = new Set(set1);
+
+  for (const value of set2) {
+    result.delete(value);
+  }
+
+  return result;
+};
+
+// 배열과 Set 객체 받아 차집합 구해서 배열로 내보내기
+const getAllDifference = function(iter1, iter2) {
+  iter1 = new Set([...iter1]);
+  iter2 = new Set([...iter2]);
+
+  let result = new Set(iter1);
+
+  for (const value of iter2) {
+    result.delete(value);
+  }
+
+  return [...result];
+};
+
+console.log(getDifference(set1, set2)); // Set { 1, 2, 3, 4, 5 }
+console.log(getAllDifference(set1, set2)); // [ 1, 2, 3, 4, 5 ]
