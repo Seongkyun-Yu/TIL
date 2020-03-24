dec94a04686b07846c785094aed291b2ce4633c1
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-24 14:19:54 +0900

    feat: Make insertionSort Func

--- javascript_study/Exercise3.js
+++ javascript_study/Exercise3.js
@@ -1,3 +1,4 @@
+// 선택 정렬
 function selectionSort(array) {
   let minIndex;
   let temp;
@@ -21,3 +22,24 @@ console.log(selectionSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
 console.log(selectionSort([2, 4, 5, 1, 3]));     // [1, 2, 3, 4, 5]
 console.log(selectionSort([5, 2, 1, 3, 4, 6]));  // [1, 2, 3, 4, 5, 6]
 
+
+// 삽입 정렬
+function insertionSort(array) {
+  let temp;
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j > 0; j--) {
+      if (array[j] < array[j - 1]) {
+        temp = array[j - 1];
+        array[j - 1] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+
+  return array;
+}
+
+console.log(insertionSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
+console.log(insertionSort([2, 4, 5, 1, 3]));     // [1, 2, 3, 4, 5]
+console.log(insertionSort([5, 2, 1, 3, 4, 6]));  // [1, 2, 3, 4, 5, 6]
