6068120139278909daec3296ecc1debdef9bce4b
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-25 14:15:17 +0900

    feat: Make Deduplication func

--- javascript_study/reduce.js
+++ javascript_study/reduce.js
@@ -6,3 +6,21 @@ const count = fruits.reduce((acc, cur) => {
 }, {});
 
 console.log(count); // { banana: 1, apple: 2, orange: 2 }
+
+
+const values = [1, [2, 3], 4, [5, 6]];
+
+const flat = values.reduce((acc, cur) => acc.concat(cur), []);
+
+console.log(flat);
+
+
+
+const values2 = [1, 2, 1, 3, 5, 4, 5, 3, 4, 4];
+
+const result = values2.reduce((acc, cur, i, arr) => {
+  if (arr.indexOf(cur) === i)  acc.push(cur);
+  return acc;
+}, []);
+
+console.log(result);
