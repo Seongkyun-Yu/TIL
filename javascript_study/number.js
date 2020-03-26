bb7a6c8583e1be2a9c38a23ded4d0fdc65250ce6
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-26 15:02:47 +0900

    feat: Use Number.MAX_VALUE

--- javascript_study/number.js
+++ javascript_study/number.js
@@ -5,6 +5,7 @@ const y = new Number('Hello');
 console.log(x); // 123
 console.log(y); // NaN
 
+
 // Number.EPSILON 자바스크립트에서 표현할 수 있는 가장 작은 수
 function isEqual(a, b) {
   return Math.abs(a - b) < Number.EPSILON;
@@ -12,3 +13,7 @@ function isEqual(a, b) {
 
 console.log(0.1 + 0.2 == 0.3);  // false
 console.log(isEqual(0.1 + 0.2, 0.3)); // true
+
+
+// Number.MAX_VALUE 자바스크립트에서 사용 가능한 가장 큰 숫자
+console.log(Number.MAX_VALUE);  // 1.7976931348623157e+308
