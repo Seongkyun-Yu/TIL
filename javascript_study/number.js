2cac0d94fc39e69c5bf272bd6a9cf77bac4052c1
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-26 14:59:18 +0900

    feat: Make isEqual func(use Number.EPSILON)

--- javascript_study/number.js
+++ javascript_study/number.js
@@ -5,3 +5,10 @@ const y = new Number('Hello');
 console.log(x); // 123
 console.log(y); // NaN
 
+// Number.EPSILON 자바스크립트에서 표현할 수 있는 가장 작은 수
+function isEqual(a, b) {
+  return Math.abs(a - b) < Number.EPSILON;
+}
+
+console.log(0.1 + 0.2 == 0.3);  // false
+console.log(isEqual(0.1 + 0.2, 0.3)); // true
