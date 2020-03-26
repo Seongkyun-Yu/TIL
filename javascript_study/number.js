42463a3dc1ac101d50375a2d07b4f491ac6f9b6a
Seongkyun-Yu <ysungkyun@gmail.com>
2020-03-26 19:05:46 +0900

    feat: Make Number method example

--- javascript_study/number.js
+++ javascript_study/number.js
@@ -26,3 +26,28 @@ console.log(Number.MIN_VALUE);  // 5e-324
 // Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY
 console.log(Number.POSITIVE_INFINITY);  // Infinity
 console.log(Number.NEGATIVE_INFINITY);  // -Infinity
+
+
+// Number Method
+// Number.isFinite 인수를 변환하지 않고 유한수인지 검사한다.
+console.log(Number.isFinite('2')); // false
+console.log(isFinite('2')); // true
+
+// Number.isInteger 인수를 숫자로 변환하지 않고 정수인지 검사한다.
+console.log(Number.isInteger('12')); // false
+
+// Number.isNaN 인수가 NaN인지 검사한다.
+console.log(Number.isNaN(NaN)); // true
+
+// Number.isSafeInteger() 안전한 정수인지 검사한다.
+console.log(Number.isSafeInteger(1000000000000000));
+console.log(Number.isSafeInteger(10000000000000001));
+
+
+// Number.prototype.toExponential 지수표기법으로 변환
+const num = 77.1234;
+console.log(num.toExponential()); // 7.71234e+1
+console.log(num.toExponential(2)); // 7.71e+1
+
+// Number.prototype.toFixed 매개변수로 지정된 소숫점자리를 반올림하여 문자열로 반환한다.
+console.log(num.toFixed()); // 
