8dc89856a833aa98e8a4feec8f4a54aab26c0558
Seongkyun-Yu <ysungkyun@gmail.com>
2020-04-26 21:57:17 +0900

    feat: Make array, queue examples

--- algorithm_study/data_structure/array.py
+++ algorithm_study/data_structure/array.py
@@ -29,11 +29,11 @@ dataset = ['Braund, Mr. Owen Harris',
 'Emir, Mr. Farred Chehab',
 'Fortune, Mr. Charles Alexander',
 'Dwyer, Miss. Ellen "Nellie"',
-'Todoroff, Mr. Lalio']
+'Todoroff, Mr. Lalio'];
 
-m_count = 0
+m_count = 0;
 for data in dataset:
     for index in range(len(data)):
         if data[index] == 'M':
-            m_count += 1
-print (m_count)
+            m_count += 1;
+print(m_count);
