9a89fac9015ddcba10434264033dea97642ea9d9
Seongkyun-Yu <ysungkyun@gmail.com>
2020-04-27 21:50:58 +0900

    feat: Make delete linked list example

--- algorithm_study/data_structure/linkedlist2.py
+++ algorithm_study/data_structure/linkedlist2.py
@@ -12,6 +12,22 @@ class NodeMgnt:
     while node.next:
       node = node.next
     node.next = Node(data)
+
+  def delete(self, data):
+    if self.head.data == data:
+      temp = self.head
+      self.head = self.head.next
+      del temp
+    else:
+      node = self.head
+      while node.next:
+        if node.next.data == data:
+          temp = node.next
+          node.next = node.next.next
+          del temp
+          return
+        else:
+          node = node.next
   
   def desc(self):
     node = self.head
@@ -21,7 +37,8 @@ class NodeMgnt:
 
 test = NodeMgnt(0)
 
-for index in range(1,10):
+for index in range(1,2):
   test.add(index)
 
+test.delete(1)
 test.desc()
