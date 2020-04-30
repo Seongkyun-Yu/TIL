212d40de16b2c8ad2310833064f6b9d3b83dbcaa
Seongkyun-Yu <ysungkyun@gmail.com>
2020-04-30 22:03:40 +0900

    fix: fix typo

--- algorithm-study/programers/level1/kthnumber.js
+++ algorithm-study/programers/level1/kthnumber.js
@@ -1,9 +1,9 @@
 function solution(array, commands) {
   return commands.map(command => {
-      let newArray = array.slice(command[0]-1, command[1]);      
-      newArray = newArray.sort((a, b) => a - b);
-      return newArray[command[2]-1];
-  })
+    let newArray = array.slice(command[0] - 1, command[1]);
+    newArray = newArray.sort((a, b) => a - b);
+    return newArray[command[2] - 1];
+  });
 }
 
 console.log(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]));
