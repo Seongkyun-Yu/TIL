31f0c27992928301e348b968c2152a7c59dd0ede
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-03 23:27:37 +0900

    fix typo

--- algorithm-study/programers/level1/duplicated.js
+++ algorithm-study/programers/level1/duplicated.js
@@ -17,12 +17,12 @@
 // 문제의 예시와 같습니다.
 
 function solution(arr) {
-    const answer = [];
-    answer.push(arr[0]);
+  const answer = [];
+  answer.push(arr[0]);
 
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] !== answer[answer.length - 1]) answer.push(arr[i]);
-    }
-    
-    return answer;
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] !== answer[answer.length - 1]) answer.push(arr[i]);
+  }
+  
+  return answer;
 }
