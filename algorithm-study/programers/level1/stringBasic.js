e74397d19d0e8d2f77b8d631736e7347fd7cb7eb
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-04 21:18:50 +0900

    feat: Make string basic control

---
+++ algorithm-study/programers/level1/stringBasic.js
@@ -0,0 +1,16 @@
+// 문제 설명
+// 문자열 s의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수, solution을 완성하세요. 예를 들어 s가 a234이면 False를 리턴하고 1234라면 True를 리턴하면 됩니다.
+
+// 제한 사항
+// s는 길이 1 이상, 길이 8 이하인 문자열입니다.
+// 입출력 예
+// s	return
+// a234	false
+// 1234	true
+
+function solution(s) {
+  const regExr4 = /^\d{4}$/;
+  const regExr6 = /^\d{6}$/;
+
+  return regExr4.test(s) || regExr6.test(s);
+}
