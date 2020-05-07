9789799bb52f4f87454cf8533cfb7e9fe63827ce
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-07 12:59:06 +0900

    feat: Make login funcs

--- react-study/chapter4/src/App.js
+++ react-study/chapter4/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  inputId = () => {
+    console.log('사용자 아이디 입력중');
+  };
+
+  inputPass = () => {
+    console.log('사용자 패스워드 입력중');
+  };
+
+  login = (e) => {
+    e.preventDefault();
+    console.log('로그인 시도');
+  };
+
+  render() {
+    return (
+      <>
+        <h1 className="h1">로그인</h1>
+        <form action="" name="id" method="post">
+          <fieldset>
+            <strong>ID:</strong>
+            <br />
+            <input
+              type="text"
+              name="id"
+              placeholder="아이디"
+              onKeyUp={this.inputId}
+            />
+            <br />
+            <strong>PASS:</strong>
+            <br />
+            <input type="password" name="password" onKeyUp={this.inputPass} />
+            <br />
+            <input
+              type="submit"
+              value="로그인하기"
+              onClick={this.login}
+              className="loginBtn"
+            />
+          </fieldset>
+        </form>
+      </>
+    );
+  }
 }
 
 export default App;
