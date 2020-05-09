20242664f2b544fe93f899d628a2135e0839729f
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-10 02:21:56 +0900

    feat: Make Login Page template component

--- react-study/chapter5/src/templates/LoginPage.js
+++ react-study/chapter5/src/templates/LoginPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Header from '../organisms/Header';
 import LoginSection from '../organisms/LoginSection';
 
-const LoginPage = ({ isLogin, logIn }) => {
+const LoginPage = ({ isLogin, loginId, logIn, logOut }) => {
   return (
     <>
-      <Header isLogin={isLogin} />
+      <Header isLogin={isLogin} loginId={loginId} logOut={logOut} />
       <LoginSection logIn={logIn} />
     </>
   );
