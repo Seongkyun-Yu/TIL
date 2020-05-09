561a10e19b3f93965a9e2434b820efb5a46da49e
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-10 02:17:58 +0900

    feat: Make LogoutBtn func

---
+++ react-study/chapter5/src/atoms/LogoutBtn.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const buttonStyle = {
+  width: '80px',
+  height: '30px',
+};
+
+const LogoutBtn = ({ logOut }) => {
+  return (
+    <button onClick={logOut} style={buttonStyle}>
+      로그아웃
+    </button>
+  );
+};
+
+export default LogoutBtn;
