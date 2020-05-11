7bc7f1903a5f55ef764fd2c6bbf897ea2abec898
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-11 20:12:52 +0900

    fix: fix typo

--- react-study/first-react-project/src/App.js
+++ react-study/first-react-project/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </>
   );
 }
 
