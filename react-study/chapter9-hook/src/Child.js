73ffd415eb28699950a879eb326323a94d482d4f
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-13 11:49:27 +0900

    feat: Make useEffect example

---
+++ react-study/chapter9-hook/src/Child.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react';
+
+const Child = ({ list, remove }) => {
+  useEffect(() => {
+    console.log('자식 컴포넌트가 화면에 나타남');
+    console.log(list);
+
+    return () => {
+      console.log('자식 컴포넌트가 화면에서 사라짐');
+      console.log(list);
+    };
+  }, [list]);
+  return <h1 onClick={() => remove(list._id)}>{list.name}</h1>;
+};
+
+export default Child;
