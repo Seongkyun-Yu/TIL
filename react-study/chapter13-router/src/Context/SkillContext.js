4efa29fac687e0d15b932d6091c35d8f8a8d486a
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-19 21:09:16 +0900

    fix: fix typo

--- react-study/chapter13-router/src/Context/SkillContext.js
+++ react-study/chapter13-router/src/Context/SkillContext.js
@@ -1,12 +1,12 @@
-import React, { useReduce, createContext } from 'react';
+import React, { useReducer, createContext } from 'react';
 import { initData, reducer } from '../Reducer/skillReducer';
 
 export const SkillContext = createContext(null);
 
 export const SkillContextProvider = ({ children }) => {
-  // const [state, dispatch] = useReduce(reducer, initData);
+  const [state, dispatch] = useReducer(reducer, initData);
 
   return (
-    <SkillContext.Provider value={initData}>{children}</SkillContext.Provider>
+    <SkillContext.Provider value={state}>{children}</SkillContext.Provider>
   );
 };
