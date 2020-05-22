118e8f0991c85a6cec716df04fcf98efe03aeede
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-22 10:01:09 +0900

    feat: Make goBack btn & css

--- react-study/movie-info/src/Components/Atoms/GoBackBtn.js
+++ react-study/movie-info/src/Components/Atoms/GoBackBtn.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './style/GoBackBtn.css';
 
 const GoBackBtn = ({ history }) => {
   const goBack = () => {
     history.goBack();
   };
 
-  return <button onClick={goBack}>BACK</button>;
+  return (
+    <button className="goBack" onClick={goBack}>
+      BACK
+    </button>
+  );
 };
 
 export default GoBackBtn;
