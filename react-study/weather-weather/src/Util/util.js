e781613e8c0cccf7eb45152b596c9fabaf461968
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-24 00:23:50 +0900

    feat: Make getYesterday func

--- react-study/weather-weather/src/Util/util.js
+++ react-study/weather-weather/src/Util/util.js
@@ -11,4 +11,19 @@ const getToday = () => {
   return year + month + date;
 };
 
-export { getToday };
+const getYesterday = () => {
+  const today = new Date();
+
+  const yesterday = new Date(today.setDate(+today.getDate() - 1));
+
+  const year = yesterday.getFullYear().toString();
+  let month = (yesterday.getMonth() + 1).toString();
+  let date = yesterday.getDate().toString();
+
+  month = month.length - 1 ? month : '0' + month;
+  date = date.length - 1 ? date : '0' + date;
+
+  return year + month + date;
+};
+
+export { getToday, getYesterday };
