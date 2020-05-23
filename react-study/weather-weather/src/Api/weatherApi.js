ccd952078bbc41d06f77b8a2d09456b58abdf309
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-23 23:48:23 +0900

    feat: Make weatherApi func

--- react-study/weather-weather/src/Api/weatherApi.js
+++ react-study/weather-weather/src/Api/weatherApi.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import axios from 'axios';
 
 const key =
-  'ZybtbxqWembBr7iOGr%2BitD88U9KStnf%2Fs0BJmKU%2BnBycJUd634Izz31eQJhsQiM%2FnLHb3I%2FoAdlMnFp8zNh31Q%3D%3D';
+  'serviceKey=ZybtbxqWembBr7iOGr%2BitD88U9KStnf%2Fs0BJmKU%2BnBycJUd634Izz31eQJhsQiM%2FnLHb3I%2FoAdlMnFp8zNh31Q%3D%3D';
 
-const weatherApi = () => {
-  return <div></div>;
+const needParams = 'pageNo=1&numOfRows=3000&dataType=JSON&base_time=0500';
+
+const weatherApi = {
+  getWeather: (today, nx, ny) =>
+    axios.get(
+      `/getVilageFcst?${key}&${needParams}&base_date=${today}&nx=${nx}&ny=${ny}`,
+    ),
 };
 
 export default weatherApi;
--- react-study/weather-weather/src/App.js
+++ react-study/weather-weather/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { getToday } from './Util/util';
+import weatherApi from './Api/weatherApi';
 
 function App() {
+  const get = async () => {
+    console.log(getToday().length);
+
+    const res = await weatherApi.getWeather(getToday(), 1, 1);
+
+    console.log(res.data.response.body.items.item[0]);
+  };
+
+  get();
+
   return <div>{getToday()}</div>;
 }
 
