f76cd6e41788b11231ebf5fbdc76aebbe787f347
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-30 00:39:59 +0900

    feat: Make failureRate func

---
+++ algorithm-study/programers/level1/failureRate.js
@@ -0,0 +1,25 @@
+function solution(N, stages) {
+  let member = stages.length;
+  let count = 0;
+  let failRatio = [];
+  for (let i = 1; i < N + 1; i++) {
+    for (let j = 0; j < stages.length; j++) {
+      if (i === stages[j]) count++;
+    }
+    if (member)
+      failRatio.push({ fail: count / member, stage: i, member, count });
+    else failRatio.push({ fail: 0, stage: i, member, count });
+
+    member -= count;
+    count = 0;
+  }
+  console.log(failRatio);
+
+  return failRatio
+    .sort((a, b) =>
+      b.fail > a.fail ? 1 : b.fail < a.fail ? -1 : a.stage < b.stage ? -1 : 1,
+    )
+    .map((fail) => fail.stage);
+}
+
+console.log(solution(8, [1, 2, 3, 4, 5, 6, 7]));
