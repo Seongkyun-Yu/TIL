bcf2622065816b52af49abcf52cb17dbf9f51108
Seongkyun-Yu <ysungkyun@gmail.com>
2020-05-30 23:09:38 +0900

    feat: Make top problem solving

---
+++ algorithm-study/programers/level1/top.js
@@ -0,0 +1,16 @@
+function solution(heights) {
+  const answer = [0];
+  for (let i = 0; i < heights.length; i++) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (heights[i] < heights[j]) {
+        console.log(i, j, heights[i], heights[j]);
+
+        answer.push(j + 1);
+        break;
+      } else if (j <= 0) answer.push(0);
+    }
+  }
+
+  return answer;
+}
+console.log(solution([6, 9, 5, 7, 4]));
