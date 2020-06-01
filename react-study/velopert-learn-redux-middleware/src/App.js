1686a3b9e8aba78a24688c5de499838947345a80
Seongkyun-Yu <ysungkyun@gmail.com>
2020-06-01 22:46:28 +0900

    feat: add saga examples

--- react-study/velopert-learn-redux-middleware/src/App.js
+++ react-study/velopert-learn-redux-middleware/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import CounterContainer from './Containers/CounterContainer';
 import SampleContainer from './Containers/SampleContainer';
 
 function App() {
   return (
     <div className="App">
-      <SampleContainer />
+      <CounterContainer />
     </div>
   );
 }
--- react-study/velopert-learn-redux-middleware/src/index.js
+++ react-study/velopert-learn-redux-middleware/src/index.js
@@ -4,12 +4,21 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-import rootReducer from './Modules';
+import rootReducer, { rootSaga } from './Modules';
 import { createLogger } from 'redux-logger';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(logger, ReduxThunk, sagaMiddleware)),
+);
+
+sagaMiddleware.run(rootSaga);
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
