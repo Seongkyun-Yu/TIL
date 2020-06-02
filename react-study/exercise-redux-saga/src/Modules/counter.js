7b9f840c437f36964ce499fcc09fdb9bd7db868c
Seongkyun-Yu <ysungkyun@gmail.com>
2020-06-02 21:48:47 +0900

    feat: Make counter reducer

---
+++ react-study/exercise-redux-saga/src/Modules/counter.js
@@ -0,0 +1,41 @@
+import { put, delay, takeEvery, takeLatest } from 'redux-saga';
+
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+const INCREASE_ASYNC = 'counter/INCREASE_ASYNC';
+const DECREASE_ASYNC = 'counter/DECREASE_ASYNC';
+
+const initialState = 0;
+
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+export const increaseAsync = () => ({ type: INCREASE_ASYNC });
+export const decreaseAsync = () => ({ type: DECREASE_ASYNC });
+
+function* increaseSaga() {
+  yield delay(1000);
+  yield put(increase());
+}
+
+function* decreaseSaga() {
+  yield delay(1000);
+  yield put(decrease());
+}
+
+export function* countersaga() {
+  yield takeEvery(INCREASE_ASYNC, increaseSaga);
+  yield takeEvery(DECREASE_ASYNC, decreaseSaga);
+}
+
+export default function counterReducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return state + 1;
+    case DECREASE:
+      return state - 1;
+    default:
+      return state;
+  }
+}
