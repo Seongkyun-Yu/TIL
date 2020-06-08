82db8a4bdc160f2db5290392af42201c8d2321e8
Seongkyun-Yu <ysungkyun@gmail.com>
2020-06-08 19:58:10 +0900

    feat: Make typescript-study project

---
+++ typescript-study/src/practice2.ts
@@ -0,0 +1,16 @@
+function sum(x: number, y: number): number {
+  return x + y;
+}
+
+const result = sum(1, 2);
+
+function sumArray(numbers: number[]): number {
+  return numbers.reduce((acc, cur) => acc + cur, 0);
+}
+
+const total = sumArray([1, 2, 3, 4, 5]);
+console.log(total);
+
+function returnNothing(): void {
+  console.log("어쩌고저쩌고");
+}
