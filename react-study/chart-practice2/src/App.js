b2c87959ee8d2d37106410869d3b3b141726ce44
Seongkyun-Yu <ysungkyun@gmail.com>
2020-08-24 17:42:02 +0900

    fix: fix something

--- react-study/chart-practice2/src/App.js
+++ react-study/chart-practice2/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainChart from './MainChart';
 
 function App() {
+  useEffect(() => {});
   return (
     <div className="App">
       <MainChart />
