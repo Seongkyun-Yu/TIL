d9a872d888fffb7e459dd6cfc338c1d78f37c441
Seongkyun-Yu <ysungkyun@gmail.com>
2020-09-16 01:07:39 +0900

    fix: no data no render

--- react-study/movie-info/src/Components/Pages/Detail.js
+++ react-study/movie-info/src/Components/Pages/Detail.js
@@ -4,24 +4,10 @@ import { useParams } from 'react-router-dom';
 import { MovieContext } from '../../Context/MovieContext';
 import DetailTemplate from '../Templates/DetailTemplate';
 
-const Detail = ({ location, history }) => {
+const Detail = ({ history }) => {
   const context = useContext(MovieContext);
   const { getDetailMovie, clearDetailMovie } = context;
   const params = useParams();
-  // getDetailMovie(params.idx);
-
-  // console.log(params.idx);
-  // console.log(detailMovies);
-
-  // console.log(params.movie_type);
-  // console.log(state[params.movie_type].results[params.movie_index]);
-
-  // const movie = location.state;
-  // console.log(movie);
-
-  // const goBack = () => {
-  //   history.goBack();
-  // };
 
   useEffect(() => {
     getDetailMovie(params.idx);
@@ -32,16 +18,6 @@ const Detail = ({ location, history }) => {
   }, []);
 
   return <DetailTemplate history={history} />;
-
-  // return (
-  //   <>
-  //     <Poster url={movie.poster_path} title={movie.title} />
-  //     <h2>{movie.title}</h2>
-  //     <p>{movie.overview}</p>
-  //     <span>개봉일: {movie.release_date}</span>
-  //     <button onClick={goBack}>뒤로 돌아가기</button>
-  //   </>
-  // );
 };
 
 export default Detail;
--- react-study/movie-info/src/Components/Templates/DetailTemplate.js
+++ react-study/movie-info/src/Components/Templates/DetailTemplate.js
@@ -13,27 +13,27 @@ const DetailTemplate = ({ history }) => {
   const context = useContext(MovieContext);
   const { state } = context;
   const { detailMovies } = state;
-
-  const goBack = () => {
-    history.goBack();
-  };
+  console.log(detailMovies);
 
   return (
-    <div className="detailContainer">
-      <Poster
-        url={detailMovies.poster_path}
-        title={detailMovies.original_title}
-      />
-      <div className="descriptContainer">
-        <MovieTitle>{detailMovies.original_title}</MovieTitle>
-        <Description>{detailMovies.overview}</Description>
-        <SpanText className="releaseDate">
-          Release Date: {detailMovies.release_date}
-        </SpanText>
-        {/* <button onClick={goBack}>뒤로 돌아가기</button> */}
-        <GoBackBtn history={history} />
-      </div>
-    </div>
+    <>
+      {Object.keys(detailMovies).length && (
+        <div className="detailContainer">
+          <Poster
+            url={detailMovies.poster_path}
+            title={detailMovies.original_title}
+          />
+          <div className="descriptContainer">
+            <MovieTitle>{detailMovies.title}</MovieTitle>
+            <Description>{detailMovies.overview}</Description>
+            <SpanText className="releaseDate">
+              Release Date: {detailMovies.release_date}
+            </SpanText>
+            <GoBackBtn history={history} />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
