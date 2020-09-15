9c8555ed9fef4947bb387dd2ec5041308e2920b6
Seongkyun-Yu <ysungkyun@gmail.com>
2020-09-15 21:59:31 +0900

    fix: fix root api directory

--- react-study/movie-info/src/api/movieApi.js
+++ react-study/movie-info/src/api/movieApi.js
@@ -5,10 +5,8 @@ const api = axios.create({
   // params: { api_key: "10923b261ba94d897ac6b81148314a3f", language: "en-US" },
 });
 
-//react-native와는 달리 로컬 웹에서 날릴 땐 쿼리스트링으로 키값을 보내라 하여 변경 하였습니다.
 const keyQuery = 'api_key=10923b261ba94d897ac6b81148314a3f';
 
-//참고해서 사용해보세요
 export const movieApi = {
   getMovie: (id) =>
     api.get(`movie/${id}?${keyQuery}`, {
