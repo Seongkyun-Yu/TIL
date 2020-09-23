e6867c892499e50ea9e79587d6e391a894d1650f
Seongkyun-Yu <ysungkyun@gmail.com>
2020-09-23 21:58:41 +0900

    feat: Make hash changin ver example

---
+++ algorithm-study/data-structure/hash2.py
@@ -0,0 +1,33 @@
+hash_table = list([0 for i in range(8)])
+
+
+# Chaining 기법으로 충돌 회피하는 해시
+def get_key(data):
+  return hash(data)
+
+def hash_function(key):
+  return key % 8
+
+def save_data(data, value):
+  index_key = get_key(data)
+  hash_address = hash_function(index_key)
+  if hash_table[hash_address] != 0:
+    for index in range(len(hash_table[hash_address])):
+      if hash_table[hash_address][index][0] == index_key:
+        hash_table[hash_address][index][1] = value
+        return
+      hash_table[hash_address].append([index_key, value])
+  else:
+    hash_table[hash_address] = [[index_key, value]]
+    
+def read_data(data):
+  index_key = get_key(data)
+  hash_address = hash_function(index_key)
+
+  if hash_table[hash_address] != 0:
+    for index in range(len(hash_table[hash_address])):
+      if hash_table[hash_address][index][0] == index_key:
+        return hash_table[hash_address][index][1]
+      return None
+  else:
+    return None
