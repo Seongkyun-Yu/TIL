cbc31b4ba5734d66e8c5e7035911da1c75e4a349
Seongkyun-Yu <ysungkyun@gmail.com>
2020-09-26 20:50:48 +0900

    feat: Make heap move_up func

--- algorithm-study/data-structure/heap.py
+++ algorithm-study/data-structure/heap.py
@@ -4,6 +4,16 @@ class Heap:
     self.heap_array.append(None)
     self.heap_array.append(data)
 
+  def move_up(self, inserted_idx):
+    if inserted_idx <= 1:
+      return False
+    
+    parent_idx = inserted_idx // 2
+    if self.heap_array[inserted_idx] > self.heap_array[parent_idx]:
+      return True
+    else:
+      return False
+
   def insert(self, data):
     if len(self.heap_array) == 0:
       self.heap_array.append(None)
@@ -11,5 +21,13 @@ class Heap:
       return True
     
     self.heap_array.append(data)
+
+    inserted_idx = len(self.heap_array) - 1
+
+    while(self.move_up(inserted_idx)):
+      parent_idx = inserted_idx // 2
+      self.heap_array[inserted_idx], self.heap_array[parent_idx] = self.heap_array[parent_idx], self.heap_array[inserted_idx]
+      inserted_idx = parent_idx
+
     return True
       
