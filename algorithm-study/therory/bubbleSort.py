78616d24063bb8c76108e6cac42c19ebe6d65f40
Seongkyun-Yu <ysungkyun@gmail.com>
2020-10-22 19:11:44 +0900

    feat: Make bubbleSort

---
+++ algorithm-study/therory/bubbleSort.py
@@ -0,0 +1,19 @@
+import random
+
+data_list = random.sample(range(100), 50)
+print(len(data_list))
+
+def bubbleSort(data):
+  for index1 in range(len(data)-1):
+    swap = False
+    for index2 in range(len(data) - index1 - 1):
+      if data[index2] > data[index2 + 1]:
+        data[index2], data[index2 + 1] = data[index2 + 1], data[index2]
+        swap = True
+
+    if swap == False:
+      break
+
+  return data
+
+print(bubbleSort(data_list))
