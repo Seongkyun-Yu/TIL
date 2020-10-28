38b7de0643a06c73a3f33041068b3f92b6852536
Seongkyun-Yu <ysungkyun@gmail.com>
2020-10-28 22:19:50 +0900

    feat: Make db input example

--- node-study/node-express-study3/controllers/admin/admin.ctrl.js
+++ node-study/node-express-study3/controllers/admin/admin.ctrl.js
@@ -1,3 +1,5 @@
+const models = require('../../models');
+
 exports.get_products = (_, res) => {
   res.render(
     'admin/products.html',
@@ -10,5 +12,12 @@ exports.get_products_write = (_, res) => {
 };
 
 exports.post_products_write = (req, res) => {
-  res.send(req.body);
+  // res.send(req.body);
+  models.Products.create({
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+  }).then(() => {
+    res.redirect('/admin/products');
+  });
 };
