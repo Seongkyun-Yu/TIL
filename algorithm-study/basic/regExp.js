4745d8c0ad88f21497fb5c71f90a3422c79d9a62
Seongkyun-Yu <ysungkyun@gmail.com>
2021-01-13 00:42:47 +0900

    feat: Make password regExp

--- algorithm-study/basic/regExp.js
+++ algorithm-study/basic/regExp.js
@@ -8,3 +8,13 @@ regExp = /^[0-9a-zA-Z]*@[0-9a-zA-Z]*.[a-zA-Z]{2,3}$/i;
 regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/;
 
 console.log(regExp.test(mail));
+
+const password = '1234!5678badd90123aa';
+regExp = /[0-9a-zA-Z]{8,10}/;
+regExp = /^[a-z0-9!~@#$%^&*()?+=/]{6,16}$/;
+const regExp2 = /[a-zA-Z]{2,3}/g;
+console.log(regExp2.test(password));
+
+// : 숫자, 특문 각 1회 이상, 영문은 2개 이상 사용하여 8자리 이상 입력
+const regExpPw = /(?=.*\d{1,50})(?=.*[~`!@#$%\^&*()-+=]{1,50})(?=.*[a-zA-Z]{2,50}).{8,50}$/;
+console.log(regExpPw.test(password));
