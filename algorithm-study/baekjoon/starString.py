0678f16dcb53f08947ea03fa3f3294c8e0165f77
Seongkyun-Yu <ysungkyun@gmail.com>
2021-02-07 04:20:54 +0900

    Algorithm: starString

--- algorithm-study/baekjoon/starString.py
+++ algorithm-study/baekjoon/starString.py
@@ -2,26 +2,44 @@
 
 n = int(input())
 
-def starString(count):
-  if count < 3:
-    return "*"
+starsArr = [[" " for _ in range(n)] for _ in range(n)]
 
-  coreStr = starString(count/3)
-
-  newStarStr = ""
-
+def drawStar(size, x, y):
+  if size == 1:
+    starsArr[y][x] = "*"
+    return
+  
   for i in range(3):
-    print(i)
     for j in range(3):
-      if(i % 2 & j % 2):
-        newStarStr += coreStr.replace("*", " ")
-      else:
-        newStarStr += coreStr
-    if(i != 2):
-      newStarStr+= "\n"
-    print(newStarStr)
+      if i == 1 & j == 1:
+        continue
+      drawStar(size//3, x + j*size//3, y + i*size//3)
+
+drawStar(n, 0, 0)
+
+for i in starsArr:
+  print("".join(i))
+
+# def starString(count):
+#   if count < 3:
+#     return "*"
+
+#   coreStr = starString(count/3)
+
+#   newStarStr = ""
+
+#   for i in range(3):
+#     print(i)
+#     for j in range(3):
+#       if(i % 2 & j % 2):
+#         newStarStr += coreStr.replace("*", " ")
+#       else:
+#         newStarStr += coreStr
+#     if(i != 2):
+#       newStarStr+= "\n"
+#     print(newStarStr)
   
 
-  return newStarStr
+#   return newStarStr
 
-print(starString(n))
+# print(starString(n))
