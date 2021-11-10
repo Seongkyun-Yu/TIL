39cb907f2e0e34fa5f1d14e5888dade91b4898ee
Seongkyun-Yu <ysungkyun@gmail.com>
2021-11-10 19:44:14 +0900

    feat: leetcode #217

---
+++ algorithm-study/leetcode/containsDuplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+    const numSet = new Set(nums);
+    
+    return numSet.size !== nums.length;
+};
