9c05e33c2524d73da4924e2b2565dbd1eaf8c524
Seongkyun-Yu <ysungkyun@gmail.com>
2021-12-11 01:53:50 +0900

    feat: leetcode 344

---
+++ algorithm-study/leetcode/reverseString.js
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/reverse-string/submissions/
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+ var reverseString = function(s) {
+    return s.reverse()
+};
