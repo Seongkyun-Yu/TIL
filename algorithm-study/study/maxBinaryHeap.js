2d63fc80ad2039f4c821b90afe55dbc2e9697241
Seongkyun-Yu <ysungkyun@gmail.com>
2022-03-26 22:05:07 +0900

    feat: Max binary heap

---
+++ algorithm-study/study/maxBinaryHeap.js
@@ -0,0 +1,21 @@
+class MaxBinaryHeap {
+    constructor(){
+        this.values = [];
+    }
+    insert(element){
+        this.values.push(element);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+}
