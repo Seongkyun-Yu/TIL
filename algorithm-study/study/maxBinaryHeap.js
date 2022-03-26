3f14632a1cf93ad84e740f70c387fd8a1ec63234
Seongkyun-Yu <ysungkyun@gmail.com>
2022-03-26 22:34:01 +0900

    feat: Add extractMax to MaxBinaryHeap

--- algorithm-study/study/maxBinaryHeap.js
+++ algorithm-study/study/maxBinaryHeap.js
@@ -18,4 +18,60 @@ class MaxBinaryHeap {
             idx = parentIdx;
         }
     }
+    extractMax(){
+        const max = this.values[0];
+        const end = this.values.pop();
+
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+        }
+
+        return max;
+    }
+    sinkDown() {
+        let idx = 0;
+        const length = this.values.length;
+        const element = this.values[0];
+        while(true){
+            let leftChildIdx = 2 * idx + 1;
+            let rightChildIdx = 2 * idx + 2;
+            let leftChild;
+            let rightChild;
+            let swap = null;
+
+            if(leftChild < length) {
+                leftChild = this.values[leftChildIdx];
+                if (leftChild > element) {
+                    swap = leftChildIdx;
+                }
+            }
+            if(rightChildIdx < length) {
+                rightChild = this.values[rightChildIdx];
+                if (
+                    (swap === null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if(swap === null) break;
+            this.values[idx] = this.values[swap];
+            this.values[swap] = element;
+            idx = swap;
+        }
+    }
 }
+
+let heap = new MaxBinaryHeap();
+heap.insert(41);
+heap.insert(39);
+heap.insert(33);
+heap.insert(18);
+heap.insert(27);
+heap.insert(12);
+heap.insert(55);
+
+console.log(heap.values)
+console.log(heap.extractMax())
+console.log(heap.values)
