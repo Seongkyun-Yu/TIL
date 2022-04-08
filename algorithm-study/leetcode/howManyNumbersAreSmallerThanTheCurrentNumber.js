5262b661793535e1ad5b7cb1bbb3e0d6ecf3934f
Seongkyun-Yu <ysungkyun@gmail.com>
2022-04-08 22:03:30 +0900

    feat: leetcode 1365

---
+++ algorithm-study/leetcode/howManyNumbersAreSmallerThanTheCurrentNumber.js
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var smallerNumbersThanCurrent = function(nums) {
+    const result = [];
+    
+    for(let i = 0; i < nums.length; i++) {
+        let count = 0;
+        
+        for(let j = 0; j <nums.length; j++) {
+            if (nums[i] > nums[j]) count++;
+        }
+        result.push(count);
+    }
+    
+    return result;
+};
