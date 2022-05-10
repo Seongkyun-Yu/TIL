7185cb0776ded9d67c53b8574cbd876ae0242ca1
Seongkyun-Yu <ysungkyun@gmail.com>
2022-05-10 22:02:16 +0900

    feat: leetcode 1450

---
+++ algorithm-study/leetcode/numberOfStudentsDoingHomeworkAtGivenTime.js
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/number-of-students-doing-homework-at-a-given-time/
+ * @param {number[]} startTime
+ * @param {number[]} endTime
+ * @param {number} queryTime
+ * @return {number}
+ */
+ var busyStudent = function(startTime, endTime, queryTime) {
+    let count = 0;
+    for(let i = 0; i < startTime.length; i++) {
+        if (startTime[i] <= queryTime && queryTime <= endTime[i]) count++;
+    }
+    
+    return count;
+};
