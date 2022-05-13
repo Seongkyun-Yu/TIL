8470397463211137c73a57147fb3c839030b2769
Seongkyun-Yu <ysungkyun@gmail.com>
2022-05-13 23:41:27 +0900

    feat: leetcode 445

---
+++ algorithm-study/leetcode/addTwoNumbers2midium.js
@@ -0,0 +1,70 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+ var addTwoNumbers = function(l1, l2) {
+    const list1 = [];
+    const list2 = [];
+    
+    let cur = l1;
+    while(cur !== null) {
+        list1.push(cur.val);
+        cur = cur.next;
+    }
+    
+    cur = l2;
+    while(cur !== null) {
+        list2.push(cur.val);
+        cur = cur.next;
+    }
+    
+    list1.reverse();
+    list2.reverse();
+    
+    const sumList = [];
+    let over10 = 0;
+    const len = list1.length > list2.length ? list1.length : list2.length;
+    for(let i = 0; i < len; i++) {
+        let sum = 0;
+        if (list1[i] && list2[i]) sum = over10 + list1[i] + list2[i]            
+        else if (!list1[i]) sum = over10 + list2[i]
+        else sum = over10 + list1[i]
+
+        if (sum >= 10) {
+            over10 = Math.floor(sum / 10);
+            sumList.push(sum - over10 * 10);
+        } else {
+            sumList.push(sum);
+            over10 = 0;
+        }
+        
+    }
+    if (over10) sumList.push(over10);
+    sumList.reverse();
+    
+    const result = new ListNode();
+    cur = result;
+    for(let i = 0; i < sumList.length; i++) {
+        cur.next = new ListNode(sumList[i]);
+        cur = cur.next;
+    }
+    
+    return result.next;
+};
+function ListNode(val, next) {
+     this.val = (val===undefined ? 0 : val)
+     this.next = (next===undefined ? null : next)
+}
+
+const l1 = new ListNode(1);
+const l2 = new ListNode(9, new ListNode(9));
+
+console.log(addTwoNumbers(l1, l2))
