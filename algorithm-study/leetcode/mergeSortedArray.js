255bd51dfc07263cd9ad9e189c8fa7ab9e121fa8
Seongkyun-Yu <ysungkyun@gmail.com>
2022-05-19 23:24:34 +0900

    fix: leetcode 88

--- algorithm-study/leetcode/mergeSortedArray.js
+++ algorithm-study/leetcode/mergeSortedArray.js
@@ -5,15 +5,21 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function (nums1, m, nums2, n) {
-  const result = [];
+ var merge = function(nums1, m, nums2, n) {
+  let len = nums1.length - 1
+  m--;
+  n--;
 
-  while (result.length < m + n) {
-    if (nums1[0] !== 0 && nums1[0] < nums2[0]) result.push(nums1.shift());
-    else result.push(nums2.shift());
+  while (n >= 0) {
+    if (nums1[m] > nums2[n]) {
+      nums1[len] = nums1[m]
+      m--;
+    } else {
+      nums1[len] = nums2[n]
+      n--;
+    }
+
+    len--;
   }
-
-  return result;
+  return nums1;
 };
-
-console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
