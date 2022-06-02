3cfd734b88a36145f410a775009e23bf2d1cd8fb
Seongkyun-Yu <ysungkyun@gmail.com>
2022-06-02 21:59:13 +0900

    fix: leetcode 121

--- algorithm-study/leetcode/bestTimeToBuyAndSellStock.js
+++ algorithm-study/leetcode/bestTimeToBuyAndSellStock.js
@@ -1,15 +1,20 @@
 /**
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
  * @param {number[]} prices
  * @return {number}
  */
  var maxProfit = function(prices) {
     if (prices.length < 2) return 0;
     
+    let buyPrice = Infinity;
     let result = 0;
     
     for (let i = 0; i < prices.length; i++) {
+        if (buyPrice <= prices[i]) continue;
         for (let j = i + 1; j < prices.length; j++) {
-            const profit = prices[j] - prices[i];
+            buyPrice = prices[i];
+            
+            const profit = prices[j] - buyPrice;
             
             if (profit > result) result = profit;
         }
