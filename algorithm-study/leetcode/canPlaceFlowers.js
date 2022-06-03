7986b75dd334c2f447cef7f0ac6b851c347a4c70
Seongkyun-Yu <ysungkyun@gmail.com>
2022-06-04 00:49:53 +0900

    feat: leetcode 605

---
+++ algorithm-study/leetcode/canPlaceFlowers.js
@@ -0,0 +1,18 @@
+/**
+ * https://leetcode.com/problems/can-place-flowers/
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+ var canPlaceFlowers = function(flowerbed, n) {
+    let count = n;
+    
+    for(let i = 0; i < flowerbed.length; i++) {
+        if (!flowerbed[i] && !flowerbed[i - 1] && !flowerbed[i + 1]) {
+            flowerbed[i] = 1;
+            count--;
+        }
+    }
+    
+    return count <= 0;
+};
