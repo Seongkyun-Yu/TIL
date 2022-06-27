ad1ff721736f4f8f5e4a5355a84120428f590af1
Seongkyun-Yu <ysungkyun@gmail.com>
2022-06-27 23:09:59 +0900

    feat: leetcode 42

--- algorithm-study/leetcode/trappingRainWater.js
+++ algorithm-study/leetcode/trappingRainWater.js
@@ -1,30 +1,32 @@
 /**
- * https://leetcode.com/problems/trapping-rain-water/
+ * https://leetcode.com/problems/trapping-rain-water
  * @param {number[]} height
  * @return {number}
  */
  var trap = function(height) {
+    const leftMaxHeight = [];
+    const rightMaxHeight = [];
     let waterHeight = 0;
     
-    for(let i = 0; i < height.length;) {
-        if (height[i] === 0) continue;
-        
-        let acc = 0;
-        let closed = false;
-        for(let j = i + 1; j < height.length; j++) {
-            if (height[i] > height[j]) {
-                acc += height[i] - height[j];
-                continue;
-            }
-            
-            i = j;
-            closed = true;
-            waterHeight += acc;
-            break;
+    for(let i = 0; i < height.length; i++) {
+        if (height[i - 1] === undefined) {
+            leftMaxHeight[i] = 0;
+            continue;
         }
-        
-        if (closed === false) i++;
-        acc = 0;
+        leftMaxHeight[i] = Math.max(leftMaxHeight[i - 1], height[i - 1]);
+    }
+    
+    for(let i = height.length - 1; i >= 0; i--) {
+        if (height[i + 1] === undefined) {
+            rightMaxHeight[i] = 0;
+            continue;
+        }
+        rightMaxHeight[i] = Math.max(rightMaxHeight[i + 1], height[i + 1]);
+    }
+    
+    for(let i = 0; i < height.length; i++) {
+        const h = Math.min(leftMaxHeight[i], rightMaxHeight[i]) - height[i];
+        waterHeight += h > 0 ? h : 0;
     }
     
     return waterHeight;
