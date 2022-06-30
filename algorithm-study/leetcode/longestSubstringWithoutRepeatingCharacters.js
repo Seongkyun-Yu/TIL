a23e6f898e80ae136996af4d56018155718d9c00
Seongkyun-Yu <ysungkyun@gmail.com>
2022-07-01 01:08:59 +0900

    feat: leetcode 3

--- algorithm-study/leetcode/longestSubstringWithoutRepeatingCharacters.js
+++ algorithm-study/leetcode/longestSubstringWithoutRepeatingCharacters.js
@@ -22,3 +22,24 @@
     
     return resultStr.length
 };
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLongestSubstring2 = function(s) {
+    if (s.length === 0) return 0;
+    
+    const chars = new Set();
+    let maxLength = 0;
+    for(let i = 0, j = 0; j < s.length; j++) {
+       while(chars.has(s[j])) {
+           chars.delete(s[i]);
+           i++;
+       }
+        chars.add(s[j])
+        maxLength = Math.max(j - i + 1, maxLength);
+    };
+    
+    return maxLength;
+};
