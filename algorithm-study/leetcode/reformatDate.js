2ffea19da5196b149239163a3e7182d55aa90717
Seongkyun-Yu <ysungkyun@gmail.com>
2022-07-11 22:52:11 +0900

    Feat: leetcode 1507

---
+++ algorithm-study/leetcode/reformatDate.js
@@ -0,0 +1,8 @@
+/**
+ * https://leetcode.com/problems/reformat-date/
+ * @param {string} date
+ * @return {string}
+ */
+ var reformatDate = function(date) {
+    return new Date(Date.parse(date.replace(/.. /, ''))).toISOString().slice(0, 10);
+};
