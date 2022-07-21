b80a8cf5a71617039e382d25d2b5b3597c135140
Seongkyun-Yu <ysungkyun@gmail.com>
2022-07-22 00:24:36 +0900

    Feat: leetcode 1550

---
+++ algorithm-study/leetcode/threeConsecutiveOdds.js
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/three-consecutive-odds/
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+ var threeConsecutiveOdds = function(arr) {
+    let count = 0;
+    
+    for(let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2) count++;
+        else count = 0;
+        
+        if (count >= 3) return true;
+    }
+    
+    return false;
+};
