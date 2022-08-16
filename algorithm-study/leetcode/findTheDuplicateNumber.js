614e65468ad923ff52abe14b38d95f549a8e7a2e
Seongkyun-Yu <ysungkyun@gmail.com>
2022-08-16 22:22:25 +0900

    Feat: leetcode 287

---
+++ algorithm-study/leetcode/findTheDuplicateNumber.js
@@ -0,0 +1,12 @@
+/**
+ * https://leetcode.com/problems/find-the-duplicate-number/
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findDuplicate = function(nums) {
+    nums.sort((a, b) => a - b);
+    
+    for(let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) return nums[i];
+    }
+};
