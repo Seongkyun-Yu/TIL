85f94991a6343a6a4e995c9392d0770b774f185a
Seongkyun-Yu <ysungkyun@gmail.com>
2022-10-25 22:56:29 +0900

    Feat: leetcode 283

--- algorithm-study/leetcode/moveZeroes.js
+++ algorithm-study/leetcode/moveZeroes.js
@@ -20,3 +20,16 @@
         nums.push(zero[i]);
     }
 };
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var moveZeroes = function(nums) {
+    for(let i = 0, zeroPos = 0; i < nums.length; i++) {
+        if (nums[i] !== 0){ 
+            [nums[i], nums[zeroPos]] = [nums[zeroPos], nums[i]];
+            zeroPos++
+        }
+    }
+};
