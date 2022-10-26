cf5281318888a14a6bb0b92aa991fc945f71695d
Seongkyun-Yu <ysungkyun@gmail.com>
2022-10-26 23:49:32 +0900

    Fix: leetcode 409

--- algorithm-study/leetcode/longestPalindrome.js
+++ algorithm-study/leetcode/longestPalindrome.js
@@ -1,19 +1,17 @@
 /**
- * https://leetcode.com/problems/longest-palindrome/
  * @param {string} s
  * @return {number}
  */
  var longestPalindrome = function(s) {
-    let left = 0;
-    let right = s.length - 1;
-    let count = 0;
-    
-    while(left < right) {
-        if (s[left] === s[right]) count += 2;
-        else count = 0;
-        left++;
-        right--;
+    const m = {};
+    for (let i = 0; i < s.length; i++) {
+        m[s[i]] = m[s[i]] + 1 || 1;
     }
-    
-    return count;
+    let numOfOdds = 0;
+    for (const key in m) {
+        if (m[key] % 2 === 1) {
+            numOfOdds += 1;
+        }
+    }
+    return numOfOdds > 0 ? s.length - numOfOdds + 1 : s.length;
 };
