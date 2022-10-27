2522a78b71a39408861f7a8ae12bfd93eea0d9fe
Seongkyun-Yu <ysungkyun@gmail.com>
2022-10-27 21:12:19 +0900

    Fix: leetcode 876

--- algorithm-study/leetcode/middleOfTheLinkedList.js
+++ algorithm-study/leetcode/middleOfTheLinkedList.js
@@ -21,3 +21,26 @@
     
     return nodes[Math.floor(nodes.length / 2)];
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var middleNode = function(head) {    
+    let slow = head;
+    let fast = head;
+    while(true) {
+        if (fast.next && !fast.next.next) return slow.next;
+        else if (!fast.next) return slow;
+        fast = fast.next?.next;
+        slow = slow.next;
+    }
+};
