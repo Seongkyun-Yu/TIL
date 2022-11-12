861bb624a8a3f2a4bf996721b7f647530bcd82d5
Seongkyun-Yu <ysungkyun@gmail.com>
2022-11-13 01:04:39 +0900

    Feat: leetcode 85

--- algorithm-study/leetcode/maximalRectangleSize.js
+++ algorithm-study/leetcode/maximalRectangleSize.js
@@ -1,46 +1,44 @@
 /**
+ * https://leetcode.com/problems/maximal-rectangle/description/
  * @param {character[][]} matrix
  * @return {number}
  */
- var maximalRectangle = function(matrix) {
-    const histograms = [];
-    let maxArea = -Infinity;
-    
-    for(let i = 0; i < matrix.length; i++) {
-        const heights = [];
+ var maximalRectangle = function(matrix) {    
+    const checkHistogram = (heights) => {
+        let maxArea = 0;
         const stack = [];
-        for(let j = 0; j < matrix[i].length; j++) {
-            console.log({stack})
-            if (matrix[i][j] === "0") {
-                heights.push(0);
-                while(stack.length) {
-                    const { h, index } = stack.pop();
-                    maxArea = Math.max(maxArea, (j - index) * h);
-                    console.log(maxArea, (j - index) * h, {j, index, h})
-                }
-                continue;
-            }
-            
-            let prevHeight = i > 0 && histograms[i - 1][j] ? histograms[i - 1][j] : 0;
-            const height = prevHeight + 1;
-            
-            while(stack[stack.length - 1]?.height > height) {
-                const { h, index } = stack.pop();
-                maxArea = Math.max(maxArea, (j - index) * h);
-                console.log(maxArea, (j - index) * h, j, index, h);
-            }
-            
-            stack.push({ h: height, index: j });
-            heights.push(height);
-        }
         
-        const width = matrix[i].length;
+        for(let i = 0; i < heights.length; i++) {
+            let startIndex = i;
+            const height = heights[i];
+            while(stack.length && stack[stack.length - 1]?.h > height) {
+                const { h, index } = stack.pop();
+                maxArea = Math.max(maxArea, (i - index) * h);
+                startIndex = index;
+            }
+            
+            stack.push({ h: height, index: startIndex });
+        }
+    
         while(stack.length) {
             const { h, index } = stack.pop();
-            maxArea = Math.max(maxArea, (width - index) * h);
+            maxArea = Math.max(maxArea, (heights.length - index) * h);
         }
-        
-        histograms.push(heights);
+    
+        return maxArea;
+    }
+
+
+    const heights = new Array(matrix[0].length).fill(0);
+    let maxArea = 0;
+    
+    for(let i = 0; i < matrix.length; i++) {
+        for(let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === "0") heights[j] = 0;
+            else heights[j]++;
+        }
+        const area = checkHistogram(heights);
+        maxArea = Math.max(maxArea, area);
     }
     
     return maxArea;
