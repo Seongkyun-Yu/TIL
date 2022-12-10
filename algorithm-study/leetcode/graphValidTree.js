26b1fd7357bef9cbf55634addb07598eb0ff931d
Seongkyun-Yu <ysungkyun@gmail.com>
2022-12-11 00:35:06 +0900

    Feat: MEDIUM - leetcode 261

--- algorithm-study/leetcode/graphValidTree.js
+++ algorithm-study/leetcode/graphValidTree.js
@@ -6,6 +6,7 @@
  * @return {boolean}
  */
 var validTree = function (n, edges) {
+  if (n === 1) return true;
   const visited = {};
   const graph = {};
 
@@ -17,22 +18,26 @@ var validTree = function (n, edges) {
     else graph[to] = [from];
   }
 
-  const dfs = (prev, i) => {
+  let visitCount = 0;
+  const dfs = (prev, i, acc) => {
     if (visited[i]) return false;
-
+    console.log(i);
     visited[i] = true;
+    visitCount++;
+
     for (const edge of graph[i]) {
       if (edge === prev) continue;
-      if (!dfs(i, edge)) return false;
+      if (!dfs(i, edge, acc + 1)) return false;
     }
-    visited[i] = false;
 
     return true;
   };
 
   for (let i = 0; i < n; i++) {
+    if (visited[i]) continue;
     if (!graph[i]) return false;
     if (!dfs(null, i)) return false;
+    if (visitCount !== n) return false;
   }
 
   return true;
