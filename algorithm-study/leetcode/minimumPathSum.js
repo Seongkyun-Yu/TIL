8603c5cd4cff16da78c9d0f300f606cd1aef050a
Seongkyun-Yu <ysungkyun@gmail.com>
2023-01-13 22:51:13 +0900

    Feat: MEDIUM - leetcode 64

--- algorithm-study/leetcode/minimumPathSum.js
+++ algorithm-study/leetcode/minimumPathSum.js
@@ -1,26 +1,22 @@
 /**
+ * MEDIUM
  * https://leetcode.com/problems/minimum-path-sum/
  * @param {number[][]} grid
  * @return {number}
  */
- var minPathSum = function(grid) {
-    let min = Infinity;
-    const rowEndIndex = grid.length - 1;
-    const colEndIndex = grid[0].length - 1;
-    
-    const findPath = (row, col, sum) => {
-        const acc = sum + grid[row][col];
-        
-        if (row === rowEndIndex && col === colEndIndex) {
-            if (min > acc) min = acc;
-            return;
-        }
-        
-        if (row < rowEndIndex) findPath(row + 1, col, acc);
-        if (col < colEndIndex) findPath(row, col + 1, acc);
+var minPathSum = function (grid) {
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      if (i - 1 < 0 && j - 1 < 0) continue;
+      if (i - 1 < 0) {
+        grid[i][j] = grid[i][j] + grid[i][j - 1];
+      } else if (j - 1 < 0) {
+        grid[i][j] = grid[i][j] + grid[i - 1][j];
+      } else {
+        grid[i][j] = grid[i][j] + Math.min(grid[i - 1][j], grid[i][j - 1]);
+      }
     }
-    
-    findPath(0, 0, 0);
-    
-    return min;
+  }
+
+  return grid[grid.length - 1][grid[0].length - 1];
 };
