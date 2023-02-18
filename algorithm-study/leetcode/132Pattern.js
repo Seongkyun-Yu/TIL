6227c50737b34e969d581088284bedbe06c1c9c7
Seongkyun-Yu <ysungkyun@gmail.com>
2023-02-19 00:27:24 +0900

    Feat: MEDIUM - leetcode 456

--- algorithm-study/leetcode/132Pattern.js
+++ algorithm-study/leetcode/132Pattern.js
@@ -4,41 +4,27 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
 var find132pattern = function (nums) {
   if (nums.length < 3) return false;
 
   let min = nums[0];
-  let max = nums[nums.length - 1];
-
   const leftMin = [min];
   for (let i = 1; i < nums.length; i++) {
     min = Math.min(min, nums[i]);
     leftMin.push(min);
   }
 
-  let rightMin = Infinity;
-  const rightMax = [];
+  const stack = [];
   for (let i = nums.length - 1; i >= 0; i--) {
-    if (nums[i] > leftMin[i - 1]) {
-      rightMin = Math.min(rightMin, nums[i]);
-      rightMax.unshift(rightMin);
-    } else if (rightMin !== Infinity) {
-      rightMax.unshift(rightMin);
-    } else rightMax.unshift(Infinity);
-  }
+    if (nums[i] <= leftMin[i]) continue;
 
-  for (let i = 1; i < nums.length - 1; i++) {
-    if (
-      leftMin[i - 1] < nums[i] &&
-      nums[i] > rightMax[i + 1] &&
-      leftMin[i - 1] < rightMax[i + 1]
-    ) {
-      return true;
+    while (stack.length && stack[stack.length - 1] <= leftMin[i]) {
+      stack.pop();
     }
+
+    if (stack[stack.length - 1] < nums[i]) return true;
+
+    stack.push(nums[i]);
   }
 
   return false;
