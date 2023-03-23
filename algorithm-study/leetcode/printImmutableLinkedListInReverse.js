5ccb5fa6cb6438e5995a451c3c07202f97dc427b
Seongkyun-Yu <ysungkyun@gmail.com>
2023-03-23 23:10:36 +0900

    Feat: MEDIUM - leetcode 1265

---
+++ algorithm-study/leetcode/printImmutableLinkedListInReverse.js
@@ -0,0 +1,28 @@
+/**
+ * MEDIUM
+ * https://leetcode.com/problems/print-immutable-linked-list-in-reverse/
+ * // This is the ImmutableListNode's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function ImmutableListNode() {
+ *    @ return {void}
+ *    this.printValue = function() { // print the value of this node.
+ *       ...
+ *    };
+ *
+ *    @return {ImmutableListNode}
+ *    this.getNext = function() { // return the next node.
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {ImmutableListNode} head
+ * @return {void}
+ */
+var printLinkedListInReverse = function (head) {
+  if (!head) return;
+
+  printLinkedListInReverse(head.getNext());
+  head.printValue();
+};
