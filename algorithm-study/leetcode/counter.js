23a5ca249c7d7a6ad7bffcaa39f0a58534e3b2f2
Seongkyun-Yu <ysungkyun@gmail.com>
2023-05-07 20:31:59 +0900

    Feat: Add prettier

--- algorithm-study/leetcode/counter.js
+++ algorithm-study/leetcode/counter.js
@@ -4,16 +4,16 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
+var createCounter = function (n) {
   let count = n;
-  return function() {
-      return count++;
+  return function () {
+    return count++;
   };
 };
 
-/** 
-* const counter = createCounter(10)
-* counter() // 10
-* counter() // 11
-* counter() // 12
-*/
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
