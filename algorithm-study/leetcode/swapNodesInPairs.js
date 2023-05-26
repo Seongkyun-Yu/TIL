78b5fd546a1db1f7c738c15c8d171a6142cff896
Seongkyun-Yu <ysungkyun@gmail.com>
2023-05-26 23:07:18 +0900

    Feat: MEDIUM - leetcode 24

--- algorithm-study/leetcode/swapNodesInPairs.js
+++ algorithm-study/leetcode/swapNodesInPairs.js
@@ -10,21 +10,45 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var swapPairs = function(head) {
-    if(!head || !head.next) return head;
-    
-    const tempHead = new ListNode();    
-    let slow = tempHead;
-    let fast = head;
+var swapPairs = function (head) {
+  if (!head || !head.next) return head;
 
-    while(fast && fast.next) {
-        slow.next = fast.next;
-        fast.next = slow.next.next;
-        slow.next.next = fast;
-        
-        slow = fast;
-        fast = slow.next;
-    }
-        
-    return tempHead.next;
+  const tempHead = new ListNode();
+  let slow = tempHead;
+  let fast = head;
+
+  while (fast && fast.next) {
+    slow.next = fast.next;
+    fast.next = slow.next.next;
+    slow.next.next = fast;
+
+    slow = fast;
+    fast = slow.next;
+  }
+
+  return tempHead.next;
+};
+
+var swapPairs2 = function (head) {
+  if (!head || !head.next) return head;
+
+  const newHead = head.next;
+
+  let prev = null;
+  let cur = head;
+  let next = cur.next;
+  while (next) {
+    if (prev) prev.next = next;
+
+    const newTarget = next.next;
+    next.next = cur;
+    cur.next = newTarget;
+
+    prev = cur;
+
+    cur = newTarget;
+    next = newTarget?.next;
+  }
+
+  return newHead;
 };
