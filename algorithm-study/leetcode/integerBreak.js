1850b1c152021f72a97ee0a0e165aca9872dd7cc
Seongkyun-Yu <ysungkyun@gmail.com>
2023-06-13 00:55:27 +0900

    Feat: MEDIUM - leetcode 343 by dp

--- algorithm-study/leetcode/integerBreak.js
+++ algorithm-study/leetcode/integerBreak.js
@@ -21,3 +21,18 @@ var integerBreak = function (n) {
 
   return dfs(n);
 };
+
+var integerBreak2 = function (n) {
+  const dp = [0, 1];
+
+  for (let num = 2; num <= n; num++) {
+    dp[num] = num === n ? 0 : num;
+
+    for (let i = 1; i < num; i++) {
+      const temp = dp[i] * dp[num - i];
+      dp[num] = Math.max(dp[num], temp);
+    }
+  }
+
+  return dp[n];
+};
