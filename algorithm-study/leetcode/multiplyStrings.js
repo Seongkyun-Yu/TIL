51ab87dbae9d14c5253b6c1c65b0e98878587c1f
Seongkyun-Yu <ysungkyun@gmail.com>
2023-06-24 23:23:47 +0900

    Feat: MEDIUM - leetcode 43

---
+++ algorithm-study/leetcode/multiplyStrings.js
@@ -0,0 +1,27 @@
+/**
+ * MEDIUM
+ * https://leetcode.com/problems/multiply-strings/
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function (num1, num2) {
+  if (num1 === '0' || num2 === '0') return '0';
+
+  const m = num1.length;
+  const n = num2.length;
+  const res = new Array(m + n).fill(0);
+
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      const p1 = i + j;
+      const p2 = i + j + 1;
+      const sum = res[p2] + Number(num1[i]) * Number(num2[j]);
+      res[p2] = sum % 10;
+      res[p1] += Math.floor(sum / 10);
+    }
+  }
+  if (res[0] === 0) res.shift();
+
+  return res.join('');
+};
