75618eb35f1f30f0e57a922ebe090bd39716d392
Seongkyun-Yu <ysungkyun@gmail.com>
2023-06-25 23:24:04 +0900

    Feat: EASY - leetcode 2469

---
+++ algorithm-study/leetcode/convertTheTemperature.js
@@ -0,0 +1,9 @@
+/**
+ * EASY
+ * https://leetcode.com/problems/convert-the-temperature/
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function (celsius) {
+  return [celsius + 273.15, celsius * 1.8 + 32];
+};
