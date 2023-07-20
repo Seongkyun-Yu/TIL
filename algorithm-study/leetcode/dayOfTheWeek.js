b4ea17d2c71e2ffa754def929acaf449950007f6
Seongkyun-Yu <ysungkyun@gmail.com>
2023-07-20 22:28:14 +0900

    Feat: EASY - leetcode 1185

---
+++ algorithm-study/leetcode/dayOfTheWeek.js
@@ -0,0 +1,13 @@
+/**
+ * EASY
+ * https://leetcode.com/problems/day-of-the-week/
+ * @param {number} day
+ * @param {number} month
+ * @param {number} year
+ * @return {string}
+ */
+var dayOfTheWeek = function (day, month, year) {
+  const date = new Date(year, month - 1, day);
+
+  return date.toLocaleDateString('en-US', { weekday: 'long' });
+};
