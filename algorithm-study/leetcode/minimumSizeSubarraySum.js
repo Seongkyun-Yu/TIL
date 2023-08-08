6ca26b3d82820b03c4fde8eadd730a7a8a4bff1d
Seongkyun-Yu <ysungkyun@gmail.com>
2023-08-08 22:48:17 +0900

    Feat: MEDIUM - leetcode 209

--- algorithm-study/leetcode/minimumSizeSubarraySum.js
+++ algorithm-study/leetcode/minimumSizeSubarraySum.js
@@ -4,20 +4,40 @@
  * @param {number[]} nums
  * @return {number}
  */
- var minSubArrayLen = function(target, nums) {
-    let minLen = Infinity;
-    let sum = 0;
-    
-    let j = 0;
-    for (let i = 0; i < nums.length; i++) {
-        sum += nums[i];
-              
-        while (sum >= target) {
-            minLen = Math.min(minLen, i - j + 1);
-            sum -= nums[j];
-            j++;
-        }
+var minSubArrayLen = function (target, nums) {
+  let minLen = Infinity;
+  let sum = 0;
+
+  let j = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+
+    while (sum >= target) {
+      minLen = Math.min(minLen, i - j + 1);
+      sum -= nums[j];
+      j++;
     }
-    
-    return minLen === Infinity ? 0 : minLen;
+  }
+
+  return minLen === Infinity ? 0 : minLen;
+};
+
+var minSubArrayLen2 = function (target, nums) {
+  let len = Infinity;
+  let sum = 0;
+  let left = 0;
+  let right = 0;
+  while (left <= right && right < nums.length) {
+    sum += nums[right];
+
+    while (sum >= target) {
+      len = Math.min(right - left + 1, len);
+      sum -= nums[left];
+      left++;
+    }
+
+    right++;
+  }
+
+  return len === Infinity ? 0 : len;
 };
