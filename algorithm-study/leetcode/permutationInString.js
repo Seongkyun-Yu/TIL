c107d7da26308d6d2a6bd9d809d96282e3b0bc73
Seongkyun-Yu <ysungkyun@gmail.com>
2023-08-19 23:01:46 +0900

    Feat: MEDIUM - leetcode 567

--- algorithm-study/leetcode/permutationInString.js
+++ algorithm-study/leetcode/permutationInString.js
@@ -3,79 +3,29 @@
  * @param {string} s2
  * @return {boolean}
  */
-//  var checkInclusion = function(s1, s2) {
-//     const s1Map = { total: 0 };
-    
-//     for(const c of s1) {
-//         s1Map[c] = (s1Map[c] || 0) + 1;
-//         s1Map.total = s1Map.total + 1;
-//     }
-        
-    
-//     for(let i = 0, j = 0; j < s2.length; j++)  {
-//         if (s1Map[s2[j]] !== undefined) {
-//             s1Map[s2[j]] = s1Map[s2[j]] - 1;
+var checkInclusion = function (s1, s2) {
+  const s1Count = {};
+  for (const c of s1) {
+    s1Count[c] = s1Count[c] + 1 || 1;
+  }
 
-//             s1Map.total = s1Map.total - 1;
-//         }
+  const s2Count = {};
+  for (let i = 0; i < s1.length; i++) {
+    s2Count[s2[i]] = s2Count[s2[i]] + 1 || 1;
+  }
 
-//         if (j - i + 1 > s1.length) {
-//             if (s1Map[s2[i]] !== undefined) {
-//                 console.log(s2[i] ,s1Map[s2[i]] )
-//                 s1Map[s2[i]] += 1;
-//                 s1Map.total += 1;
-//             }
-//             i++;
-//         }
-        
-//         if (s1Map.total === 0) return true;
-        
-        
-        
-//         console.log("i", i, "j", j)
-//         console.log(s1Map)
-//     }
-    
-//     return false;
-// };
-
-/**
- * @param {string} s1
- * @param {string} s2
- * @return {boolean}
- */
- var checkInclusion = function(s1, s2) {
-    const s1Map = {};
-    const s2Map = {};
-    let matches = 0;
-    
-    for(const c of s1) {
-        s1Map[c] = s1Map[c] || 0 + 1;
-    }
-    
-    for(let left = 0, right = 0; right < s2.length; right++) {
-        s2Map[s2[right]] = s2Map[s2[right]] || 0 + 1;
-        if (s1Map[s2[right]] === s2Map[s2[right]]) matches += s2Map[s2[right]];
-        
-        if (right - left + 1 < s1.length) continue;
-
-        if (s2Map[s2[left]]) {
-            s2Map[s2[left]] -= 1;
-            if (s1Map[s2[left]]) matches += 1;
-        }
-        
-        left++;
-
-        console.log("s1Map", s1Map)
-        console.log("s2Map", s2Map)
-        console.log("matches", matches)
-        
-        if (matches === 26) {
-            return true
-        };
+  for (let i = s1.length; i <= s2.length; i++) {
+    for (let j = 0; j < s1.length; j++) {
+      if (s1Count[s1[j]] === s2Count[s1[j]]) {
+        if (j === s1.length - 1) return true;
+        continue;
+      }
+      break;
     }
 
-    return false
+    s2Count[s2[i - s1.length]] = s2Count[s2[i - s1.length]] - 1;
+    s2Count[s2[i]] = s2Count[s2[i]] + 1 || 1;
+  }
+
+  return false;
 };
-
-console.log(checkInclusion("hello","ooolleoooleh"))
